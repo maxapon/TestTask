a463925334e152eb2d6e7cbb06c3d0a822d22ab2
maxaponpatapon@gmail.com <maxaponpatapon@gmail.com>
2022-09-01 19:28:43 +0300

    Добавлены комментарии

--- Program.cs
+++ Program.cs
@@ -8,48 +8,37 @@ namespace TestTask
     {
         public static void Main(string[] args)
         {
-            //Console.WriteLine("4 задание:");
-            //Console.WriteLine(GetNumberSum(123));
-            //Console.WriteLine();
+            Console.WriteLine("4 задание:");
+            Console.WriteLine(GetNumberSum(123));
+            Console.WriteLine();
 
-            //Console.WriteLine("5 задание:");
-            //var res = GetMoneys(-144);
-            //Console.WriteLine($"N:{res["Nickels"]}; P:{res["Pennies"]}; D:{res["Dimes"]}; Q:{res["Quarters"]}");
-            //Console.WriteLine();
+            Console.WriteLine("5 задание:");
+            var res = GetMoneys(144);
+            Console.WriteLine($"N:{res["Nickels"]}; P:{res["Pennies"]}; D:{res["Dimes"]}; Q:{res["Quarters"]}");
+            Console.WriteLine();
 
-            //Console.WriteLine("6 задание:");
-            //Console.WriteLine(GetMaxFromNumber(545802));
-            //Console.WriteLine();
+            Console.WriteLine("6 задание:");
+            Console.WriteLine(GetMaxFromNumber(545802));
+            Console.WriteLine();
 
-            //Console.WriteLine("7 задание:");
-
-            //Console.WriteLine("8 задание:");
-            //Console.WriteLine(GetFive());
-            //Console.WriteLine();
-
+            Console.WriteLine("7 задание:");
             Console.WriteLine(GetPiramidRowSum(4));
+            Console.WriteLine();
+
+            Console.WriteLine("8 задание:");
+            Console.WriteLine(GetFive());
+            Console.WriteLine();
 
             Console.ReadKey();
         }
 
-        //1 - за 4 дня. Мы делим все 1000 пробирок на 10. получаем группы пробирок по 100. Даем каждой мыши по капли лекарства из каждой пробирки своей группы.
-        //Ждем сутки. Одна из мышей умерла => в этой группе и был яд. дальше делим 100 пробирок на 9 групп (8 по 11 и 1 по 12). Даем мышам по капле из каждой пробирки своей группы.
-        //Ждем сутки. Одна из мышей умерла => в этой группе и был яд. В худшем случае умрет мышь которой досталась группа из 12 пробирок. Делим 12 пробирок на 8 групп для каждой из мышек(4 - по 1 пробирки и 4 по 2).
-        //Даем каждой мышке. Ждем сутки. Одна из мышей умерла => в этой группе и был яд. В хужшем случае яд окажется в группе с 2 пробирками => повторяем процедуру и однозначно идетифицируем пробирку с ядом за 4 дня.
-        //
-        //2 - 2100 нужно отдать руководителю, поскольку в условиях стояла задача решить задачу первым. Ни синьер, ни джун не решил её первым => все деньги получает руководитель
-        //ИЛИ
-        //составляем уравнение: x + 2x + 4x = 2100. x - доля джуна. 2x - доля руководителя. 4x - доля синьера. Поскольку джун должен получить в 2 раза меньше чем руководитель
-        //4x - доля синьера, поскольку синьер должен получить в 2 раза больше руководителя. Дальше решаем уравнение и получаем x = 300$
-        //Значит джун должен получить 300, руководитель 600, синьер 1200
-        //
-        //3 - нужна 1 попытка. Поскольку на всех автоматах неправильные наклейки. Включаем автомат с наклейкой Чай\кофе.
-        //Смотрим, что получили. Если чай, тогда автомат с наклейкой чай будет наливать кофе, поскольку если автомат с наклейкой чай будет наливать чай\кофе, то не будут выполнены условия
-        //по которым все наклейки перепутаны. В автомате с наклейкой кофе будет наливаться чай\кофе по остаточному принципу.
-        //
-        //Если же в автомате с наклейкой чай\кофе появился кофе, то в автомате с наклейкой кофе будет наливаться чай, а в автомате с наклейкой чай - чай\кофе
-
+        
         //4:
+        /// <summary>
+        /// Получает сумму цифр числа до тех пор, пока количество цифр в числе не будет равно 1
+        /// </summary>
+        /// <param name="N">Число, чьи цифры необходимо ссумировать</param>
+        /// <returns>int</returns>
         public static int GetNumberSum(int N)
         {
             int result = 0;
@@ -62,6 +51,11 @@ namespace TestTask
         }
 
         //5:
+        /// <summary>
+        /// Получает минимальное количество монет из которых можно получить переданную сумму
+        /// </summary>
+        /// <param name="Cents">Сумма в центах</param>
+        /// <returns>Словарь с ключами: Nickels, Pennies, Dimes, Quarters </returns>
         public static Dictionary<string, int> GetMoneys(int Cents)
         {
             Dictionary<string, int> result = new Dictionary<string, int>(){ { "Nickels", 0 }, 
@@ -83,32 +77,54 @@ namespace TestTask
         }
 
         //6:
+        /// <summary>
+        /// Получает максимальное значение из цифр переданного числа
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
         public static int GetMaxFromNumber(int N)
         {
+            //Получаем строковое представление числа, чтобы можно перебрать каждую из цифр числа
             string strN = N.ToString();
+            //Создаем временный массив для int-овых представлений цифр числа N
             int[] tmpArr = new int[strN.Length];
+            //Заполняем временный массив
             for (int i = 0; i < strN.Length; i++)
             { 
                 tmpArr[i] = (int)Char.GetNumericValue(strN[i]);
             }
+            //Сортируем массив из цифр числа N
             Array.Sort(tmpArr);
             string res = string.Empty;
+            //Разворачиваем отсортированный массив, чтобы получить максимальное число из цифр числа N
             foreach (var i in tmpArr.Reverse())
             { 
+                //В соответствующем порядке добавляем значения к результату
                 res += i.ToString();
             }
+            //Возвращаем интовое представление результата
             return Convert.ToInt32(res);
         }
 
         //7:
+        /// <summary>
+        /// Получает сумму нечетных числе в строке пирамиды
+        /// </summary>
+        /// <param name="RowIndex">Индекс строки</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public static int GetPiramidRowSum(int RowIndex)
         {
             if (RowIndex > 0)
             {
+                //Вычисляем первое число строки
                 int startNumber = RowIndex * (RowIndex - 1) + 1;
                 int result = startNumber;
+                //Вычисляем результат, в цикле от 1, поскольку первое число было уже добавлено к результату, до индекса строки,
+                //поскольку количество нечетных чисел в строке пирамиды равно индексу строки
                 for (int i = 1; i < RowIndex; i++)
                 {
+                    //Прибавляем к стартовому числу 2, чтобы перескачить четные числа
                     startNumber += 2;
                     result += startNumber;
                 }
@@ -119,8 +135,13 @@ namespace TestTask
         }
 
         //8:
+        /// <summary>
+        /// Возвращает 5 не используя цифры
+        /// </summary>
+        /// <returns>5</returns>
         public static int GetFive()
         {
+            //Возвращает десятичное представление символа в unicode
             return '';
         }
     }
